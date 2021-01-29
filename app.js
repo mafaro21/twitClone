f3758bdee0fc5af50dd017efb5cfc0a811b92e1d
Longwater1234 <davisinyo@gmail.com>
2021-01-30 04:28:37 +0800

    fixed the port

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log(`listening on port ${process.env.PORT}`)
 })
 
