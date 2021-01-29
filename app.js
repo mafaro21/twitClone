6917873833e2ff7f4e638808d280195392182b03
Longwater1234 <davisinyo@gmail.com>
2021-01-30 03:53:57 +0800

    changed homepage

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use((err, req, res, next) => {
     // render the error page
     res.status(err.status || 500);
     res.render('error');
+    console.error('error');
 });
 
 module.exports = app;
