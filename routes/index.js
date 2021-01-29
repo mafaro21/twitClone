c525bb37feb987d7f28762f24ac45a1c53a0c7c6
Longwater1234 <davisinyo@gmail.com>
2021-01-30 03:50:04 +0800

    changes changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+    console.log('we are on home page');
 });
 
 module.exports = router;
