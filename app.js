87666730e32140c532e5c85e64074f7249203c2e
Longwater1234 <davisinyo@gmail.com>
2021-01-31 00:37:29 +0800

    minor changes

--- app.js
+++ app.js
@@ -49,10 +49,10 @@ app.post("/register", (req, res) => {
         }   
     }
     if (checkInputs() === true ) {
-         res.status(200).json(JSON.stringify(req.body));
+         res.status(200).json(req.body);
          console.json(req.body, res.sendStatus(200));
     } else {
-       res.status(422).json(JSON.stringify(errors));
+       res.status(403).json(errors);
        console.error(errors);
     }
 })
