07709408e43cac23a315d2bf896cf3ba350541eb
Longwater1234 <davisinyo@gmail.com>
2021-02-03 22:11:27 +0800

    changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'TwitClone: Home' });
+   res.render('index', { title: 'TwitClone: Home' });
    // console.log('we are on home page');
 });
 
