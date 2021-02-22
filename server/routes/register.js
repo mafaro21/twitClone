6ba86b58215131b9051fe6863936a4ec04685343
Longwater1234 <davisinyo@gmail.com>
2021-02-22 23:34:07 +0800

    added object

--- server/routes/register.js
+++ server/routes/register.js
@@ -61,7 +61,7 @@ router.post("/", (req, res, next) => {
     };
     axios.request(axiosOptions)
         .then(res => {
-            console.log(res.data);
+            console.log(res.data); // show the object
             isValid = res.data.success && (res.data.score >= 0.5); //check if both TRUE
             let prob = res.data['error-codes'];
             if (prob) console.error(prob);
