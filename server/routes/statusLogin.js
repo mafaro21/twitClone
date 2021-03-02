b936a319cfa7d495864374987269db9666007876
Longwater1234 <davisinyo@gmail.com>
2021-03-02 09:57:41 +0800

    return email instead of id

--- server/routes/statusLogin.js
+++ server/routes/statusLogin.js
@@ -7,7 +7,7 @@ router.get("/", (req, res, next) => {
     if (!req.session || !req.session.user) {
         return res.json({ "loggedin": false });
     } else
-        return res.json({ "loggedin": true, "userID": req.session.user.id });
+        return res.json({ "loggedin": true, "user": req.session.user.email });
 });
 
 module.exports = router;
