0c80cde7dfd70fc87b44a709de59dd889f19ac2d
Longwater1234 <davisinyo@gmail.com>
2021-03-05 20:10:32 +0800

    EDIT PROFILE FORM

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -15,15 +15,15 @@ router.get("/mine", isLoggedin, (req, res, next) => {
         useUnifiedTopology: true,
         useNewUrlParser: true,
     })
-    .then(client => {
-        const users = client.db('twitclone').collection('users');
-        const projection = { _id: 0, password: 0, email: 0 }; // <--exclusions
-        users.findOne({ _id: userid }, { projection: projection }, (err, result) => {
-            if (!result) res.sendStatus(404);
-            else res.json(result);
-            client.close();
-        });
-    }).catch(next);
+        .then(client => {
+            const users = client.db('twitclone').collection('users');
+            const projection = { _id: 0, password: 0, email: 0 }; // <--exclusions
+            users.findOne({ _id: userid }, { projection: projection }, (err, result) => {
+                if (!result) res.sendStatus(404);
+                else res.json(result);
+                client.close();
+            });
+        }).catch(next);
 });
 
 /*  GETTING OTHER user profile */
@@ -32,8 +32,30 @@ router.get("/user/:userid", (req, res, next) => {
     res.send(`Profile belongs to  ${userid}`);
 });
 
-/* handling UPDATE PROFILE  */
-router.put("/mine", isLoggedin, (req, res, next) => {
+/* handling UPDATE MY PROFILE  */
+router.put("/mine/edit", isLoggedin, (req, res, next) => {
+    let userid = req.session.user.id;
+    //--------------GET ALL THE VARIBLES FROM THE BODY
+    //do validation first
+    //--------------here--------------------//
+    MongoClient.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    }).then(client => {
+        const users = client.db('twitclone').collection('users');
+        const newValues = { fullname: '', username: '', bio: '' }; // <--newValues
+        users.updateOne({ _id: userid }, { $set: newValues }, (err, result) => {
+            if (err) {
+                res.status(400).send({"message": err.message, "success": false });
+                res.end();
+                console.error(err);
+            } else {
+                 //RERWITE THE SESSION VARIABLES HERE.
+                 res.json({ "message": result.modifiedCount, "success": true });
+            }
+            client.close();
+        });
+    }).catch(next)
 
 });
 
