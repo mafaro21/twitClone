b81866088859111557ffc40a3879b4fcbeec782b
Davis Tibbz <33986524+Longwater1234@users.noreply.github.com>
2021-07-28 15:01:54 +0800

    Update logout.js

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -2,7 +2,7 @@ const express = require("express");
 const isLoggedin = require("../middleware/authchecker");
 const router = express.Router();
 
-router.get("/", isLoggedin, (req, res, next) => {
+router.post("/", isLoggedin, (req, res, next) => {
         req.session.destroy((err) => {
             if (err) throw err;
             return res.send({ message: "logged out", success: true });
